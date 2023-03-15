ca41b3a7ef776ad7f78c4f56d430273efaf736d9
Loris Sauter <loris.sauter@unibas.ch>
2023-03-15 16:32:02 +0100

    Added code and plots for judgment time analysis

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Shared code for the post-hoc analysis of Video Browser Showdown 2023
 * `data/logs` Relevant logs per-team
 * `data/processed` Extracted data from `data/raw` for detailed analysis (e.g. AVS data separated from KIS data etc.)
 * `src/*` Analysis code per language (e.g. `src/julia` for Julia)
-* `plots/` Plots produced with the code in `src/*` based on the data in `data`
+* `plots/` Plots produced with the code in `src/*` based on the data in `data/*`
+* `tex/` LaTeX output, produced with code in `src/*`, based on the data in `data/*`
---
+++ src/julia/avs-judgement-analysis.jl
@@ -0,0 +1,53 @@
+using CSV, DataFrames, CairoMakie, CategoricalArrays
+using ColorSchemes, ColorBrewer
+
+juds = DataFrame(CSV.File("data/processed/avs-submissions-judgements.csv"));
+
+# Time between submission and prepare (a judge requested the submission)
+juds.deltaPrep = (juds.ptimestamp - juds.stimestamp) ./ 1000; #ms
+# Time between prepare and judgement (the time it took the judge)
+juds.deltaJudge = (juds.jtimestamp - juds.ptimestamp) ./ 1000;
+# Total time it took between submission and verdict
+juds.deltaAll = (juds.jtimestamp - juds.stimestamp) ./ 1000;
+
+# Categorise it for Makie
+juds.stask = categorical(juds.stask);
+
+fig = Figure();
+labels = replace.(levels(juds.stask), "vbs23-avs" => "a");
+ax = Axis(fig[1,1],
+xticks = ( # x ticks
+1:length(levels(juds.stask)), # numerical version of xticks, basically indices for label (next argument)
+labels),
+title="Time judges took to for their verdict",
+xlabel = "tasks",
+ylabel = "seconds"
+);
+
+# Time between prepare and judgement - the time it took for the judge
+boxplot!(ax, juds.stask.refs, juds.deltaJudge);
+save("plots/avs-judgement-time-judging.pdf", fig);
+
+# Time between judgement request and deliver (should be small)
+ax = Axis(fig[1,1],
+xticks = ( # x ticks
+1:length(levels(juds.stask)), # numerical version of xticks, basically indices for label (next argument)
+labels),
+title="Time the preparation of a judgement took",
+xlabel = "tasks",
+ylabel = "seconds"
+);
+boxplot!(ax, juds.stask.refs, juds.deltaPrep);
+save("plots/avs-judgement-time-preparing.pdf", fig);
+
+# Total time between submission and verdict
+ax = Axis(fig[1,1],
+xticks = ( # x ticks
+1:length(levels(juds.stask)), # numerical version of xticks, basically indices for label (next argument)
+labels),
+title="Time between submission and its verdict",
+xlabel = "tasks",
+ylabel = "seconds"
+);
+boxplot!(ax, juds.stask.refs, juds.deltaAll);
+save("plots/avs-judgement-time-total.pdf", fig);
