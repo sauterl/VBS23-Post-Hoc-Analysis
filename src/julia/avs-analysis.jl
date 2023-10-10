6e369003d1aea26632b3a467c623945c3ebaebf8
Loris Sauter <loris.sauter@unibas.ch>
2023-10-10 11:48:34 +0200

    Using only first correct submission

--- src/julia/avs-analysis.jl
+++ src/julia/avs-analysis.jl
@@ -24,13 +24,14 @@ df.team = replace.(df.team, "HTW" => "vibro")
 df.task = categorical(df.task);
 df.team = categorical(df.team);
 
+
 # Plotting setup
 fig = Figure();
 # Axis
 ax = Axis(fig[1,1],             # Placement within the figure [1,1] is top left
 xticks = (                      # The x ticks
     1:length(levels(df.task)),  # Numerical version of xticks, basically indices for label (next argument)
-    labels),                    # The labels to draw from (with the indices of the previous argument).
+    replace.(levels(df.task), "vbs23-avs" => "a")),                    # The labels to draw from (with the indices of the previous argument).
 title="Shares of submissions per team and task"
 );
 # Makie stacked barplot of the shares of submissions per team and task
@@ -81,7 +82,7 @@ cfig = Figure();
 cax = Axis(cfig[1,1],             # Placement within the figure [1,1] is top left
 xticks = (                      # The x ticks
 1:length(levels(cdf.task)),  # Numerical version of xticks, basically indices for label (next argument)
-labels),                    # The labels to draw from (with the indices of the previous argument).
+replace.(levels(cdf.task), "vbs23-avs" => "a")),                    # The labels to draw from (with the indices of the previous argument).
 title="Shares of submissions per team and task"
 );
 # Makie stacked barplot of the shares of submissions per team and task
@@ -101,6 +102,61 @@ team_names,     # The labels, i.e. the teams
 
 # Saving the plot on disk using default size measurements
 save("plots/avs-team-ratios-correct.pdf", cfig);
+
+# ---------------------------------------------------
+# Analysis of correct submissions with unique item (i.e. video) per team
+#   (k times video v for team t counts as 1, in case multiple teams have sumitted the same segment, this is not filtered)
+# ---------------------------------------------------
+# Group by task and team and item, count (nrow) and get the unique values on task and team
+csubsPerTaskTeamItems = unique(combine(groupby(csubs, [:task, :team, :item]), :, nrow), [:task, :team, :item]);
+# We want to only count one submission per item (technically the first, however we are calculating ratios here, so it doesn't matter
+csubsPerTaskTeamItems.nrow .= 1;
+# Sum up to get the total of the submissions per task and rename to sum
+citotals = combine(groupby(csubsPerTaskTeamItems, [:task]), :nrow .=> sum => :sum);
+# Join on the taks (name)
+cidf = innerjoin(csubsPerTaskTeamItems, citotals, on = :task);
+# Calculate ratio total per task and team / totals
+cidf.ratio = cidf.nrow ./ cidf.sum;
+# Clean Team naming
+cidf.team = replace.(cidf.team, "HTW" => "vibro")
+# Makie works better with categorical arrays, hence we convert the relevant columns to categorical
+cidf.task = categorical(cidf.task);
+cidf.team = categorical(cidf.team);
+
+cistacks = maximum(indexin(cidf.team, team_names))+1 .- indexin(cidf.team,team_names);
+
+# Plotting setup
+cifig = Figure();
+# Axis
+ciax = Axis(cifig[1,1],             # Placement within the figure [1,1] is top left
+xticks = (                      # The x ticks
+1:length(levels(cidf.task)),  # Numerical version of xticks, basically indices for label (next argument)
+replace.(levels(cidf.task), "vbs23-avs" => "a")),                    # The labels to draw from (with the indices of the previous argument).
+title="Shares of first correct submissions per team and task"
+);
+# Makie stacked barplot of the shares of submissions per team and task
+barplot!(ciax,                    # The axis
+cidf.task.refs,               # The x values, must be numerical, hence the refs. Multiple equal X values result in stacks
+cidf.ratio,                   # The Y values, must be numerical, this is the share of submissions of a team (bar) for the given task (x tick)
+stack=cistacks,         # Numerical value of stack ordering
+color=[team_colours[t] for t in cidf.team], # Numerical colour value within the theme (we use the same values as for the stacking)
+#bar_labels=df.team         # Apparently, categorical array works here, basically all the labels for all the bars (remember, they get stacked) # DISABLED, since unreadable
+strokecolor= :white, # Have a white border and see whether this makes it more readable
+#strokewidth= 0.1, # have a width for the stroke to enhance readability # this looks horrible
+#offset= 0.1,
+);
+# The Legend with reversed entries due to the way of the stacking
+Legend(cifig[1,2],       # Placement within the figure: to the right of fig[1,1]
+team_legendelements,  # Colour elements aka squares
+team_names,     # The labels, i.e. the teams
+"Teams"             # Title of the legend
+);
+
+# Saving the plot on disk using default size measurements
+save("plots/avs-team-ratios-correct-single-item.pdf", cifig);
+
+
+
 
 # ---------------------------------------------------
 # Analysis of unique submissions
